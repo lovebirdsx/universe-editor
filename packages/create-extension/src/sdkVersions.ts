f3dfec57000141b280719070471cde6a90a94802
lovebird <lovebirdsx@gmail.com>
2026-08-21 15:54:07 +0800

    chore(release): 0.13.3

--- packages/create-extension/src/sdkVersions.ts
+++ packages/create-extension/src/sdkVersions.ts
@@ -19,7 +19,7 @@ export interface SdkVersions {
 }
 
 export const SDK_VERSIONS: SdkVersions = {
-  extensionApi: '0.13.2',
+  extensionApi: '0.13.3',
   uex: '0.3.2',
   esbuild: '^0.25.0',
   typescript: '^5.8.0',
