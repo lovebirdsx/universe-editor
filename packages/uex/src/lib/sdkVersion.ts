7116b48f85592753d4d1a7743253c939a887160d
lovebird <lovebirdsx@gmail.com>
2026-09-03 17:09:52 +0800

    chore(release): 0.13.6

--- packages/uex/src/lib/sdkVersion.ts
+++ packages/uex/src/lib/sdkVersion.ts
@@ -5,4 +5,4 @@
  * engines.universe coverage warning (authors most often target the current
  * API). Derived at build time from packages/extension-api/package.json.
  */
-export const CURRENT_API_VERSION = '0.13.5'
+export const CURRENT_API_VERSION = '0.13.6'
